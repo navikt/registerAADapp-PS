2f6eb09892954ac0b8ca19eb5b8fbbf3a76d57ca
Kjetil Nordlund <kjetil@jll.no>
2018-12-04 13:59:36 +0100

    oppdatert med multi tenant kjøring + claimspolicy

--- Last working version.ps1
+++ Last working version.ps1
@@ -1,34 +1,13 @@
 #Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\MSOnline\1.1.183.8\MSOnline.psd1" -Global;
-#Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureADPreview\2.0.1.18\AzureADPreview.psd1" -Global;
-#Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.Profile\4.6.0\AzureRM.Profile.psd1" -Global;
-#Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.KeyVault\4.3.0\AzureRM.KeyVault.psd1" -Global;
-#Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.Resources\5.5.2\AzureRM.Resources.psd1" -Global;
+Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureADPreview\2.0.1.18\AzureADPreview.psd1" -Global;
+Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.Profile\4.6.0\AzureRM.Profile.psd1" -Global;
+Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.KeyVault\4.3.0\AzureRM.KeyVault.psd1" -Global;
+Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.Resources\5.5.2\AzureRM.Resources.psd1" -Global;
 #Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureAD\2.0.1.16\AzureAD.psd1" -Global;
-
-#. "D:\home\site\wwwroot\registerApp-PS\createKey.ps1"
+#Import-Module AzureADPreview
+#. .\createKey.ps1
+. "D:\home\site\wwwroot\registerApp-PS\createKey.ps1"
 #. ..\config.ps1
-
-
-if ($ENV:environment -like "local") {
-    Import-Module AzureADPreview
-    . .\registerApp-PS\createKey.ps1
-    $vaultname = "azureSelfServiceKeys"
-    $requestbody = Get-Content -Raw .\registerApp-PS\input.example.json | ConvertFrom-Json
-
-} else {
-    . "D:\home\site\wwwroot\registerApp-PS\createKey.ps1"
-    Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureADPreview\2.0.1.18\AzureADPreview.psd1" -Global;
-    Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.Profile\4.6.0\AzureRM.Profile.psd1" -Global;
-    Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.KeyVault\4.3.0\AzureRM.KeyVault.psd1" -Global;
-    Import-Module "D:\home\site\wwwroot\registerApp-PS\additionalmodules\AzureRM.Resources\5.5.2\AzureRM.Resources.psd1" -Global;
-
-    $vaultname = "azureSelfServiceKeys"
-
-    # POST method: $req
-    $requestBody = Get-Content $req -Raw | ConvertFrom-Json
-
-
-}
 
 $tenantid = $ENV:APPSETTING_tenantid
 $applicationId = $ENV:APPSETTING_applicationId
@@ -37,16 +16,15 @@ $appSecretToken = $ENV:APPSETTING_appSecretToken
 $tokenAuthURI = $ENV:APPSETTING_tokenAuthURI
 $certThumbprint = $ENV:APPSETTING_certThumbprint
 $secretWebhook = $ENV:APPSETTING_secretWebhook
-$claimswebhook =  $ENV:APPSETTING_runbookUrl
-$domainname = $ENV:APPSETTING_domainname
-$subscriptionId = $ENV:APPSETTING_subscriptionId
 
 # POST method: $req
-#$requestBody = Get-Content $req -Raw | ConvertFrom-Json
+$requestBody = Get-Content $req -Raw | ConvertFrom-Json
 
 $securePassword = $appSecret | ConvertTo-SecureString -AsPlainText -Force
 $credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $applicationId, $securePassword
 $applicationName = $requestbody.applicationName
+$vaultname = "azureSelfServiceKeys"
+$domainname = "nav.no"
 $ApplicationURI = $("https://$DomainName/$($applicationName -Replace('[\W]',''))")
 $logoutURI = "https://login.microsoftonline.com/common/oauth2/logout"
 
@@ -55,19 +33,9 @@ $logoutURI = "https://login.microsoftonline.com/common/oauth2/logout"
 $replyurls = $($requestbody.replyURLs).replace(" ",",")
 $owners = $($requestbody.owners).replace(" ",",")
 
-if ($ENV:APPSETTING_domainname -like "trygdeetaten.no") {
-    $keyvaultname = $($applicationName -Replace('[\W]','')) + "Q"
-    $ClaimsPolicy = "53eccc94-e7d4-4c1b-9d25-62baefaab0df" #53eccc94-e7d4-4c1b-9d25-62baefaab0df = preprod. c7880a6d-ab30-4fba-be66-4c41d7e6f22f = prod
-}else {
-    $keyvaultname = $($applicationName -Replace('[\W]',''))+ "Prod"
-    $ClaimsPolicy = "c7880a6d-ab30-4fba-be66-4c41d7e6f22f" #53eccc94-e7d4-4c1b-9d25-62baefaab0df = preprod. c7880a6d-ab30-4fba-be66-4c41d7e6f22f = prod
-}
-
-
-
 
 # kobler til azure ad med sertifikat som credentials
-Connect-AzureAD -TenantId  $tenantid -ApplicationId $applicationId -CertificateThumbprint $certThumbprint
+Connect-AzureAD -ApplicationId $applicationId -CertificateThumbprint $certThumbprint -TenantId $tenantid
 
 # sjekker om applikasjon med samme navn finnes fra før.
 $appExist = Get-AzureADApplication -filter "DisplayName eq '$applicationname'"
@@ -75,9 +43,10 @@ $appExist = Get-AzureADApplication -filter "DisplayName eq '$applicationname'"
 # om appen ikke finnes, registrer med alle parametere
 if (!($appExist)) {
     $GroupMembershipClaims = "SecurityGroup"
-   
+    $ClaimsPolicy = "c7880a6d-ab30-4fba-be66-4c41d7e6f22f" #53eccc94-e7d4-4c1b-9d25-62baefaab0df = preprod. c7880a6d-ab30-4fba-be66-4c41d7e6f22f = prod
+
     # kobler til azure resource manager
-    Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId $tenantid -Subscription $subscriptionId #preprod = "2f230ac3-94ea-48b6-93b9-8b1fc97add0a", prod="e9589aaa-5d01-4d93-a252-9cc27f620f44" 
+    Connect-AzureRmAccount -ServicePrincipal -Credential $credential -TenantId $tenantid -Subscription "e9589aaa-5d01-4d93-a252-9cc27f620f44"
 
     $requestBodyAD = "grant_type=client_credentials" +
         "&client_id=$applicationId" +
@@ -104,20 +73,20 @@ if (!($appExist)) {
     $reqGraph.ResourceAppId = "00000002-0000-0000-c000-000000000000"  # = windows azure active directory
     $appPermission1 = New-Object -TypeName "Microsoft.Open.AzureAD.Model.ResourceAccess" -ArgumentList "311a71cc-e848-46a1-bdf8-97ff7156d8e6","Scope"  # = User.read, delegated permission
     $reqGraph.ResourceAccess = $appPermission1
-    
+
     #regsiter application
     $app = New-AzureADApplication -IdentifierUris $ApplicationURI -DisplayName $ApplicationName -GroupMembershipClaims $GroupMembershipClaims -homepage $ApplicationURI -logoutUrl $logoutURI -PasswordCredentials $aadSecret -ReplyUrls $replyurls -RequiredResourceAccess $reqGraph
     $resServicePrincipal = New-AzureRmADServicePrincipal -ApplicationId $app.appID
     
 
-
-
     # oppretter ny keyvault
+    $keyvaultname = $($applicationName -Replace('[\W]',''))+ "Prod"
     $keyvault = New-AzureRmKeyVault -name $keyvaultname -ResourceGroupName AzureSelfServiceKeyVault -Location "North Europe"
     Set-AzureRmKeyVaultAccessPolicy -VaultName $keyvaultname -PermissionsToSecrets set -ServicePrincipalName $ApplicationId
 
     # skriver app secret til Azure keyvault
     $vaultSecret = Set-AzureKeyVaultSecret -VaultName $keyvaultname -name $keyvaultname -SecretValue $secret
+    
     
   
     
@@ -161,9 +130,9 @@ if (!($appExist)) {
         )
     }
     
-    if (!($ENV:environment = "local")) {
-        Invoke-RestMethod -uri $secretWebhook -Method Post -Body $teamsbody -ContentType 'application/json'
-    }
+    
+    Invoke-RestMethod -uri $secretWebhook -Method Post -Body $teamsbody -ContentType 'application/json'
+    
     
 # oppdaterer app med acceptmappedclaims = true
 $requestbodyAD =
@@ -174,26 +143,6 @@ $requestbodyAD =
 "@
 
 $updateApp = Invoke-RestMethod -Method Patch -Uri "https://graph.windows.net/$tenantid/applications/$($app.objectid)?api-version=1.6" -Headers @{"Authorization"="Bearer $accessTokenAD"} -body $requestBodyAD -ContentType "application/json"
-
-# Knytter til NAVident Claims Policy
-#write-host $applicationName
-#$servicePrincipalId = Get-AzureADServicePrincipal -filter "DisplayName eq '$applicationname'"
-#write-host $servicePrincipalId
-#Add-AzureADServicePrincipalPolicy -Id $servicePrincipalId.ObjectId -RefObjectId $ClaimsPolicy
-$claimsbody = @{
-    'applicationname' = $applicationName
-    'claimspolicy' = $ClaimsPolicy
-    'env' = $domainname
-   }
-
-$claimsbodyJSON = ConvertTo-Json $claimsbody
-
-$addcustomclaims = Invoke-RestMethod -uri $claimswebhook -Method POST -body $claimsbodyJSON -ContentType "application/json"
-
-
-
-
-
 
 # setter respons
 $output = ConvertTo-Json @{
@@ -214,10 +163,10 @@ $accessTokenGraph = $tokenResponseGraph.access_token
 
 # Sender mail til bruker med info om applikasjonen
 
-$content="<html>\r\n<head>\r\n<meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'>\r\n<meta content=\'text/html; charset=us-ascii\'>\r\n</head>\r\n<body>\r\n<b>Opprettelsen av Azure AD applikasjonen $applicationName er ferdig</b></br></br>appID/ClientID: $($app.appID)</br>ClientSecret: https://portal.azure.com/#@trygdeetaten.no/asset/Microsoft_Azure_KeyVault/Secret/$($vaultSecret.id)</br>objectID: $($app.ObjectId)</br></br> Mvh</br>NAIS\r\n</body>\r\n</html>\r\n"
+$content="<html>\r\n<head>\r\n<meta http-equiv=\'Content-Type\' content=\'text/html; charset=utf-8\'>\r\n<meta content=\'text/html; charset=us-ascii\'>\r\n</head>\r\n<body>\r\n<b>Opprettelsen av Azure AD applikasjonen $applicationName er ferdig</b></br></br>appID/ClientID: $($app.appID)</br>ClientSecret: https://portal.azure.com/#@nav.no/asset/Microsoft_Azure_KeyVault/Secret/$($vaultSecret.id)</br>objectID: $($app.ObjectId)</br></br> Mvh</br>NAIS\r\n</body>\r\n</html>\r\n"
 
 foreach ($owner in $owners) {
-Write-Output $owner
+#Write-Output $owner
     $emailbody = @"
         {
         "message" : {
@@ -237,14 +186,14 @@ Write-Output $owner
         }
 "@
     #Send-MailMessage -from no-reply@nav.no -to $owner -subject "Applikasjonsbestillingen er ferdig utført" -SmtpServer smtp.office365.com -port 587 -usessl -Credential $credential
-    $mailURI = "https://graph.microsoft.com/v1.0/users/kjetil.nordlund@trygdeetaten.no/sendMail"
+    $mailURI = "https://graph.microsoft.com/v1.0/users/kn@navno.onmicrosoft.com/sendMail"
     Invoke-RestMethod -Method Post -Uri $mailURI -Headers @{"Authorization"="Bearer $accessTokenGraph"} -ContentType application/json -Body $emailbody
 
 }
 
 
 $output
-#Out-File -Encoding Ascii -FilePath $res -inputObject $output
+Out-File -Encoding Ascii -FilePath $res -inputObject $output
 }
 
 
@@ -268,14 +217,14 @@ else {
     {
         $appownerComp += $appowner.UserPrincipalName
     }
-    Write-output $appownerComp
+    #Write-output $appownerComp
 
     $ownerComp = @()
     foreach ($owner in $owners)
     {
         $ownerComp += $owner
     }
-    write-output $ownerComp
+    #write-output $ownerComp
 
     $addowner = Compare-Object -ReferenceObject $appownerComp -DifferenceObject $ownerComp | where sideindicator -eq "=>"
     $removeOwner = Compare-Object -ReferenceObject $appownerComp -DifferenceObject $ownerComp | where sideindicator -eq "<="
@@ -293,8 +242,8 @@ else {
         $aduser = Get-AzureADUser -filter "UserPrincipalName eq '$($owner.inputObject)'"
         Remove-AzureADApplicationOwner -objectid $appExist.ObjectId -OwnerId $aduser.ObjectId
     }
-    Write-Output $addowner
-    Write-Output $removeOwner
+    #Write-Output $addowner
+    #Write-Output $removeOwner
 
     # leser ut nye eiere fra app
     $newowners = Get-AzureADApplicationOwner -ObjectId $appExist.ObjectId
@@ -308,5 +257,5 @@ else {
     }
     
     $output
- #   Out-File -Encoding Ascii -FilePath $res -inputObject $output
+    Out-File -Encoding Ascii -FilePath $res -inputObject $output
 }
